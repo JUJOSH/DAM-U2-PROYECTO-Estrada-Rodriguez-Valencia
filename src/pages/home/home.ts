20ebe049414d2688463f20480f1a19a57649f909
JomaEstrada <jose.estrada.madera@gmail.com>
2017-03-16 22:55:57 -0700

    Nuevo cambio 9:55

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,11 +10,16 @@ export class HomePage {
   public registrar:boolean;
   public valor:any;
   public opt:number=0;
+  public cont:number=0;
   constructor(public navCtrl: NavController) {
     
   }
    toggleRegistro(){
     console.log('test');
+    this.cont+=1;
+    if(this.cont == 2){
+      this.cont=0;
+    }
     this.opt=1;
   }
 
